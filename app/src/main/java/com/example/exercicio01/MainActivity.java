8ee4a6a49cf476047bd84b7da96df72acfaf9ac5
duartesluiza <duartes.luiza92@gmail.com>
2022-06-29 00:25:43 -0300

    alterando activity_main e Main

--- app/src/main/java/com/example/exercicio01/MainActivity.java
+++ app/src/main/java/com/example/exercicio01/MainActivity.java
@@ -15,20 +15,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText e1 = (EditText) findViewById(R.id.nome);
-        EditText e2 = (EditText) findViewById(R.id.telefone);
-        EditText e3 = (EditText) findViewById(R.id.senha);
+        EditText e1 = (EditText) findViewById(R.id.txtNome);
+        EditText e2 = (EditText) findViewById(R.id.txtEmail);
+        EditText e3 = (EditText) findViewById(R.id.txtsenha);
 
         TextView t1 = (TextView)  findViewById(R.id.titulo);
 
-        Button b1 = (Button) findViewById(R.id.bt1);
+        Button b1 = (Button) findViewById(R.id.btnOK);
         //escutar quando tiver um clique no botao
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                t1.setText(e1.getText()+"-"+e2.getText()+"-"+e3.getText());
+                final TextView lbl = (TextView) findViewById(R.id.lblResultado);
+                final EditText txtNome = (EditText) findViewById(R.id.txtNome);
+                String nome = txtNome.getText().toString();
+                lbl.setText("oi mundo." + nome +" tudo bem?");
             }
         });
-    }
 
+
+    }
 }
